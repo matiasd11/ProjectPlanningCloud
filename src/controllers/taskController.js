99c56ff525e9404e4d3b6f204230a33ab4f33507
matiasd11 <matiduran03@gmail.com>
2025-11-12 23:12:18 -0300

    increase limit

--- src/controllers/taskController.js
+++ src/controllers/taskController.js
@@ -6,7 +6,7 @@ const taskController = {
   getAllTasks: async (req, res) => {
     try {
       const page = parseInt(req.query.page) || 1;
-      const limit = parseInt(req.query.limit) || 10;
+      const limit = parseInt(req.query.limit) || 20;
       const { status, projectId } = req.query;
       const offset = (page - 1) * limit;
       
